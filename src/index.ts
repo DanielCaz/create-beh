fa8a7148d9201a11b2b28009a2a1361814bad5b8
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2023-10-10 14:53:30 -0600

    Finished v1

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+const copyTemplate = async (projectName: string) => {
+  const process = Bun.spawnSync([
+    "git",
+    "clone",
+    "https://github.com/DanielCaz/beh-template",
+    projectName,
+  ]);
+
+  if (!process.success) {
+    console.log("Error: ", process.stdout);
+    return;
+  }
+
+  const packageJson = Bun.file(`${projectName}/package.json`);
+  if (!packageJson.exists()) {
+    console.log("Error: package.json not found");
+    return;
+  }
+
+  const packageJsonContent = await packageJson.text();
+
+  await Bun.write(
+    `${projectName}/package.json`,
+    packageJsonContent.replace("beh-template", projectName)
+  );
+
+  console.log("Done");
+};
+
+console.log("Welcome to create BEH");
+console.write("Project name: ");
+
+let projectName = "beh-app";
+for await (const line of console) {
+  projectName = line;
+  break;
+}
+
+copyTemplate(projectName);
